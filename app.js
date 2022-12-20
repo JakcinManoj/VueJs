fd3639cbdd1f32328f2e47336f82ceb5077d9b13
Jakcin Manoj <manojjakcin.2001@gmail.com>
2022-12-20 18:37:38 +0530

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const inputs = document.querySelector('input');
-const uls = document.querySelector('ul');
-const buttons = document.querySelector('button');
-console.log(inputs);
-console.log(uls);
-console.log(buttons);
+// const inputs = document.querySelector('input');
+// const uls = document.querySelector('ul');
+// const buttons = document.querySelector('button');
+// console.log(inputs);
+// console.log(uls);
+// console.log(buttons);
 
-const fun = () => {
-    const enteredval = inputs.value;
-    const listele = document.createElement('li');
-    listele.textContent = enteredval;
-    uls.appendChild(listele);
-    inputs.value=' ';
-}
+// const fun = () => {
+//     const enteredval = inputs.value;
+//     const listele = document.createElement('li');
+//     listele.textContent = enteredval;
+//     uls.appendChild(listele);
+//     inputs.value=' ';
+// }
 
-buttons.addEventListener("click",fun);
+// buttons.addEventListener("click",fun);\
+
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredvalue: "",
+    };
+  },
+  methods: {
+    fun() {
+      this.goals.push(this.enteredvalue);
+    },
+  },
+}).mount("#app");

0db9d3d8a38383c442d241cc3b9df24b86a0e4d2
Jakcin Manoj <manojjakcin.2001@gmail.com>
2023-01-08 15:54:10 +0530

    added fav books only and make it false @click

--- OneDrive/Desktop/VUE/Practice/app.js
+++ OneDrive/Desktop/VUE/Practice/app.js
@@ -1,29 +1,27 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      showBooks: true,
-      title: "The Way of Kings",
-      author: "Brandon Sanderson",
-      age: 45,
-      x: 0,
-      y: 0,
-    };
-  },
-  methods: {
-    toggleShowBooks() {
-      this.showBooks = !this.showBooks;
-    },
-    handleEvent(e, data) {
-      console.log(e.type, e);
-      if (data) {
-        console.log(data);
+    data() {
+      return {
+        showBooks: true,
+        books: [
+          { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+          { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+          { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+        ],
       }
     },
-    handleMousemove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
+    methods: {
+      toggleShowBooks() {
+        this.showBooks =  !this.showBooks
+      },
+      toggleFav(book){
+        book.isFav= !book.isFav
+      }
     },
-  },
-});
-
-app.mount("#app");
+    computed: {
+        filteredBooks(){
+            return this.books.filter((book) => book.isFav)
+        }
+    }
+  })
+  
+  app.mount('#app')
